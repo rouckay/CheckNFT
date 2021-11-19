bc9ce817579d576c77cb55c43217d192ce70e17a
Farhad Rahmani <55535348+2019farhad@users.noreply.github.com>
2021-11-19 10:39:58 +0430

    Updates

---
+++ precache-manifest.628c39a87641baaade3f69f430198a4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d904bea950d61b324ab24f03aa2c614",
+    "url": "/CheckNFT/index.html"
+  },
+  {
+    "revision": "687b5679db6b6d405603",
+    "url": "/CheckNFT/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "026487e6693b55d79e02",
+    "url": "/CheckNFT/static/js/2.b6f1219b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CheckNFT/static/js/2.b6f1219b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687b5679db6b6d405603",
+    "url": "/CheckNFT/static/js/main.9e42c9a3.chunk.js"
+  },
+  {
+    "revision": "112ca5df965afdce70a0",
+    "url": "/CheckNFT/static/js/runtime-main.c4a817ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CheckNFT/static/media/logo.5d5d9eef.svg"
+  }
+]);
